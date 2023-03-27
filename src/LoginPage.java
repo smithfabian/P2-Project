60e32fac9e1837da6ff9b6e97277bf02ca4ed26a
DavidXiao1 <tmoldr22@student.aau.dk>
2023-03-27 22:38:15 +0200

    made LoginPage class

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,6 +1,16 @@
-import javafx.*;
 
-public class LoginPage {
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.*;
+
+public class LoginPage extends Application {
 
 
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
