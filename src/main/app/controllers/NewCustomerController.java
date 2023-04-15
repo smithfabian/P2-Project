6de64f0d67bcc35108771a9a4bf3c75d82eae126
DavidXiao1 <tmoldr22@student.aau.dk>
2023-04-15 23:57:03 +0200

    New customers can now be added to the databse via the add button in the SalesPage P2-135

--- src/main/app/controllers/NewCustomerController.java
+++ src/main/app/controllers/NewCustomerController.java
@@ -3,7 +3,9 @@ package main.app.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import main.app.models.NewCustomerModel;
 import main.app.models.NewOrderModel;
@@ -20,7 +22,8 @@ public class NewCustomerController {
     @FXML
     TextField subSectorField;
     @FXML
-    TextField postalField;
+    Label addedLabel;
+
     Stage stage;
     NewCustomerModel newCustomerModel;
     public NewCustomerController() {
@@ -30,11 +33,25 @@ public class NewCustomerController {
         this.stage = stage;
     }
 
-    public void cancelButtonClicked(ActionEvent actionEvent) {
+    public void cancelButtonClicked() {
         stage.close();
     }
 
-    public void addButtonClicked(ActionEvent actionEvent) {
-        newCustomerModel.addToDatabase();
+    public void addButtonClicked() {
+        if (!IDField.getText().isEmpty()) {
+            newCustomerModel.setID(IDField.getText());
+            newCustomerModel.setMainSector(mainSectorField.getText());
+            newCustomerModel.setSubSector(subSectorField.getText());
+            newCustomerModel.addToDatabase();
+            addedLabel.setText("New customer added successfully");
+            addedLabel.setTextFill(Color.GREEN);
+            IDField.clear();
+            mainSectorField.clear();
+            subSectorField.clear();
+        }
+        else {
+            addedLabel.setText("Please fill in all required text fields");
+            addedLabel.setTextFill(Color.RED);
+        }
     }
 }
