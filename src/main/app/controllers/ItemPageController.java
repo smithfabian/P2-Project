18f3fe98c517d5782e041c9ce7f9bd58f71cef0e
DavidXiao1 <tmoldr22@student.aau.dk>
2023-04-21 14:58:07 +0200

    Created ItemPage-view P2-123

--- src/main/app/controllers/ItemPageController.java
+++ src/main/app/controllers/ItemPageController.java
@@ -1,10 +1,14 @@
 package main.app.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import main.app.models.ItemPageModel;
 import main.app.models.SalesModel;
+import java.util.Date;
+import java.util.List;
 
 public class ItemPageController {
     Stage stage;
@@ -19,7 +23,10 @@ public class ItemPageController {
     Label totalBought;
     @FXML
     Label totalReturned;
-
+    @FXML
+    LineChart<String,Integer> boughtChart;
+    @FXML
+    LineChart<String, Integer> returnedChart;
 
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -29,6 +36,7 @@ public class ItemPageController {
     public void setModelValues(SalesModel.ItemRow row) {
         itemPageModel = new ItemPageModel(row);
         setLabelText();
+        fillCharts();
 
     }
 
@@ -38,5 +46,28 @@ public class ItemPageController {
         subGroup.setText("Item sub group: " + itemPageModel.getSubGroup());
         totalBought.setText("Total bought: " + itemPageModel.getTotalBought());
         totalReturned.setText("Total returned: " + itemPageModel.getTotalReturned());
+    }
+    public void fillCharts() {
+        List<Integer> boughtAxis = itemPageModel.getBoughtAxis();
+        List<Integer> returnedAxis = itemPageModel.getReturnedAxis();
+        List<String> dateAxis = itemPageModel.getDateAxis();
+        // Clear existing data
+        boughtChart.getData().clear();
+        boughtChart.getData().clear();
+
+        // Create new series
+        XYChart.Series<String, Integer> boughtSeries = new XYChart.Series<>();
+        XYChart.Series<String, Integer> returnedSeries = new XYChart.Series<>();
+
+        // Add data to series
+        for (int i = 0; i < boughtAxis.size() ; i++) {
+            boughtSeries.getData().add(new XYChart.Data<>(dateAxis.get(i),boughtAxis.get((i))));
+            returnedSeries.getData().add(new XYChart.Data<>(dateAxis.get(i),returnedAxis.get((i))));
+
+        }
+
+        // Add series to bar charts
+        boughtChart.getData().add(boughtSeries);
+        returnedChart.getData().add(returnedSeries);
     }
 }
--- src/main/app/models/ItemPageModel.java
+++ src/main/app/models/ItemPageModel.java
@@ -1,11 +1,21 @@
 package main.app.models;
 
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class ItemPageModel {
     private String itemID;
     private String mainGroup;
     private String subGroup;
     private int totalBought;
     private int totalReturned;
+    private List<Integer> boughtAxis;
+    private List<Integer> returnedAxis;
+    private List<String> dateAxis;
 
 
     public ItemPageModel(SalesModel.ItemRow row) {
@@ -14,6 +24,7 @@ public class ItemPageModel {
         this.subGroup = row.getSubGroup();
         this.totalBought = row.getTotalBought();
         this.totalReturned = row.getTotalReturned();
+        createChartData();
 
     }
 
@@ -35,5 +46,42 @@ public class ItemPageModel {
 
     public int getTotalReturned() {
         return totalReturned;
+    }
+
+    public List<String> getDateAxis() {
+        return dateAxis;
+    }
+
+    public List<Integer> getBoughtAxis() {
+        return boughtAxis;
+    }
+
+    public List<Integer> getReturnedAxis() {
+        return returnedAxis;
+    }
+
+    public void createChartData() {
+        dateAxis = new ArrayList<>();
+        boughtAxis = new ArrayList<>();
+        returnedAxis = new ArrayList<>();
+        try {
+            String query = "SELECT InvoiceDate, sum(if(InvoiceQty >= 0, InvoiceQty, 0)) as boughtOnDay, sum(if(InvoiceQty < 0, InvoiceQty, 0)) AS returnedOnDay" +
+                    " from p2.invoiceregister where ItemID = ? group by InvoiceDate order by InvoiceDate";
+            Connection conn = DatabaseConnection.getConnection();
+            try (PreparedStatement stmt = conn.prepareStatement(query)) {
+                stmt.setString(1,itemID);
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()) {
+                    dateAxis.add(rs.getString("InvoiceDate"));
+                    boughtAxis.add(rs.getInt("boughtOnDay"));
+                    returnedAxis.add(rs.getInt("returnedOnDay"));
+                }
+            } finally {
+                conn.close();
+            }
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
